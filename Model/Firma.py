40b4281fe1daef7091fee2d745a2d2f125ee8380
sebastian <sebatorin@hotmail.com>
2017-11-24 22:41:55 +0100

    Updated

--- Model/Firma.py
+++ Model/Firma.py
@@ -4,10 +4,6 @@ import json
 class Firma:
     'Firma Objekt'
 
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__,
-                          sort_keys=True, indent=4)
-
 
 class Ansprechpartner:
     'Ansprechpartner Objekt'
--- Service/Parser.py
+++ Service/Parser.py
@@ -1,19 +1,17 @@
 from Model.Firma import Firma, Ansprechpartner
 import xml.etree.ElementTree as ET
 import json
-# import the rolen in this part
 from Config.config import rollen
 
 
 def get_firmen_objects(name, branche, ort, rolle):
-    # parse the xml file
+    print(name,branche,ort,rolle)
     with open('..\\Data\\firmendaten.xml', 'r') as xml_file:
         tree = ET.parse(xml_file)
     root = tree.getroot()
     firmen = root.findall('firma')
     firmen_list = []
     if rolle in rollen:
-        print("IN ROLE")
         for firma_element in firmen:
             if firma_element.attrib['name'] == name and branche == "" and ort == "" \
                     and rolle in firma_element.attrib['name']:
@@ -36,31 +34,42 @@ def get_firmen_objects(name, branche, ort, rolle):
             elif firma_element.attrib['name'] == name and branche == "" \
                     and ort == firma_element.find('ort').text and rolle in firma_element.attrib['name']:
                 firmen_list = filter_objects(firma_element, firmen_list)
+            elif "" == name and branche == "" \
+                    and ort == "" and rolle in firma_element.attrib['name']:
+                firmen_list = filter_objects(firma_element, firmen_list)
     elif rolle not in rollen:
-        print("NOT IN ROLE")
         for firma_element in firmen:
             if firma_element.attrib['name'] == name and branche == "" and ort == "" \
-                    and firma_element.attrib['name'] not in rollen:
+                    and check_for_role(firma_element.attrib['name']):
                 firmen_list = filter_objects(firma_element, firmen_list)
             elif firma_element.attrib['name'] == name and branche == firma_element.find('branche').text and ort == "" \
-                    and firma_element.attrib['name'] not in rollen:
+                    and check_for_role(firma_element.attrib['name']):
                 firmen_list = filter_objects(firma_element, firmen_list)
             elif firma_element.attrib['name'] == name and branche == firma_element.find('branche').text \
-                    and ort == firma_element.find('ort').text and firma_element.attrib['name'] not in rollen:
+                    and ort == firma_element.find('ort').text and check_for_role(firma_element.attrib['name']):
                 firmen_list = filter_objects(firma_element, firmen_list)
             elif "" == name and branche == firma_element.find('branche').text \
-                    and ort == firma_element.find('ort').text and firma_element.attrib['name'] not in rollen:
+                    and ort == firma_element.find('ort').text and check_for_role(firma_element.attrib['name']):
                 firmen_list = filter_objects(firma_element, firmen_list)
             elif "" == name and branche == "" \
-                    and ort == firma_element.find('ort').text and firma_element.attrib['name'] not in rollen:
+                    and ort == firma_element.find('ort').text and check_for_role(firma_element.attrib['name']):
                 firmen_list = filter_objects(firma_element, firmen_list)
             elif "" == name and branche == firma_element.find('branche').text \
-                    and ort == "" and firma_element.attrib['name'] not in rollen:
+                    and ort == "" and check_for_role(firma_element.attrib['name']):
                 firmen_list = filter_objects(firma_element, firmen_list)
             elif firma_element.attrib['name'] == name and branche == "" \
-                    and ort == firma_element.find('ort').text and firma_element.attrib['name'] not in rollen:
+                    and ort == firma_element.find('ort').text and check_for_role(firma_element.attrib['name']):
                 firmen_list = filter_objects(firma_element, firmen_list)
     return json.dumps(firmen_list)
+
+
+def check_for_role(firma_name):
+    for role in rollen:
+        if role in firma_name:
+            return False
+    return True
+
+
 
 
 def filter_objects(firma_element, firmen_list):
@@ -97,5 +106,4 @@ def filter_objects(firma_element, firmen_list):
     firma = firma.__dict__
     firmen_list.append(firma)
     return firmen_list
-
 
