a5f651c62ba1e7d3fe31f054dd1effd572f6a43d
sebastian <sebatorin@hotmail.com>
2017-11-26 11:30:23 +0100

    updated filtering for roles

--- Service/Parser.py
+++ Service/Parser.py
@@ -2,9 +2,14 @@ from Model.Firma import Firma, Ansprechpartner
 import xml.etree.ElementTree as ET
 import json
 from Config.config import rollen
-import re
 
 
+# Get the list of companies according to query parameters in JSON format
+# @param name the name of the company
+# @param branche the department of the company
+# @param ort the place of the company
+# @param rolle the role the company has
+# return the list of companies queried in JSON
 def get_firmen_JSON(name, branche, ort, rolle):
     with open('..\\Data\\firmendaten.xml', 'r') as xml_file:
         tree = ET.parse(xml_file, ET.XMLParser(encoding='utf-8'))
@@ -46,8 +51,7 @@ def get_firmen_JSON(name, branche, ort, rolle):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
             elif name in name_element and branche in branche_element and ort == "" and has_not_role(name_element):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
-            elif name in name_element and branche in branche_element and ort == ort_element and has_not_role(
-                    name_element):
+            elif name in name_element and branche in branche_element and ort == ort_element and has_not_role(name_element):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
             elif "" == name and branche in branche_element and ort == ort_element and has_not_role(name_element):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
@@ -63,23 +67,33 @@ def get_firmen_JSON(name, branche, ort, rolle):
         return "[]"
     return json.dumps(firmen_list).replace("name", "@name")
 
-
+# Check if the given company has no role
+# @param firma_name the name of the company
+# return true if it has no role, false otherwise
 def has_not_role(firma_name):
     for role in rollen:
         if is_role_in_name(role, firma_name):
             return False
     return True
 
-
+# Check if the given name has a role in it
+# @param role the role to check
+# @param name the name of the company
+# return True if the company name has the specified role, false otherwise
 def is_role_in_name(role, name):
     try:
-        index = name.index(role)
-        if name[index - 1] == " ":
+        index = name.index(role, len(name) - len(role))
+        if (name[index - 1] == " ") and ((index + len(role)) % len(name) == 0):
             return True
     except:
         return False
+    else:
+        return False
 
-
+# Parse the XML Elements into objects and populate the given list
+# @param firma_element the XML root element
+# @param firmen_list the list to be populated
+# return the populated list
 def get_firmen_list(firma_element, firmen_list):
     firma = Firma()
     firma.name = firma_element.attrib['name']
--- Tests/parser_test.py
+++ Tests/parser_test.py
@@ -1,12 +1,11 @@
-role = "mbh"
-name = "Derdack gmbh"
-
+role = "ag"
+name = "agentur Deutsche Akademie ag & co."
 
 
 def is_role_in_name(role, name):
     try:
-        index = name.index(role)
-        if name[index-1] == " ":
+        index = name.index(role,len(name)-len(role))
+        if (name[index - 1] == " ") and ((index + len(role)) % len(name) == 0):
             print("yes")
     except:
         print("fail")
