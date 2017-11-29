f369431caeaa15b4024e36d88aac5536dfa145e3
sebastian <sebatorin@hotmail.com>
2017-11-29 10:13:19 +0100

    Better name convention and comments

--- Service/Parser.py
+++ Service/Parser.py
@@ -40,30 +40,30 @@ def get_firmen_JSON(name, branche, ort, rolle):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
             elif "" == name and branche == "" and ort == "" and is_role_in_name(rolle, name_element):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
-    # if the role does not exist or was not given
+    # if no role was desired
     elif rolle == "keine":
-        print("not in role")
+        print("no role")
         for firma_element in firmen:
             name_element = firma_element.attrib['name'].lower()
             branche_element = firma_element.find('branche').text.lower()
             ort_element = firma_element.find('ort').text.lower()
-            if name in name_element and branche == "" and ort == "" and has_not_role(name_element):
+            if name in name_element and branche == "" and ort == "" and has_no_role(name_element):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
-            elif name in name_element and branche in branche_element and ort == "" and has_not_role(name_element):
+            elif name in name_element and branche in branche_element and ort == "" and has_no_role(name_element):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
-            elif name in name_element and branche in branche_element and ort == ort_element and has_not_role(name_element):
+            elif name in name_element and branche in branche_element and ort == ort_element and has_no_role(name_element):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
-            elif "" == name and branche in branche_element and ort == ort_element and has_not_role(name_element):
+            elif "" == name and branche in branche_element and ort == ort_element and has_no_role(name_element):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
-            elif "" == name and branche == "" and ort == ort_element and has_not_role(name_element):
+            elif "" == name and branche == "" and ort == ort_element and has_no_role(name_element):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
-            elif "" == name and branche in branche_element and ort == "" and has_not_role(name_element):
+            elif "" == name and branche in branche_element and ort == "" and has_no_role(name_element):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
-            elif name in name_element and branche == "" and ort == ort_element and has_not_role(name_element):
+            elif name in name_element and branche == "" and ort == ort_element and has_no_role(name_element):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
-            elif "" == name and branche == "" and ort == "" and has_not_role(name_element):
+            elif "" == name and branche == "" and ort == "" and has_no_role(name_element):
                 firmen_list = get_firmen_list(firma_element, firmen_list)
-    else:
+    else:  # if role was not recognized or no role was given
         return "[]"
     return json.dumps(firmen_list).replace("name", "@name")
 
@@ -71,7 +71,7 @@ def get_firmen_JSON(name, branche, ort, rolle):
 # Check if the given company has no role
 # @param firma_name the name of the company
 # return true if it has no role, false otherwise
-def has_not_role(firma_name):
+def has_no_role(firma_name):
     for role in rollen:
         if is_role_in_name(role, firma_name.strip()):
             return False
@@ -84,12 +84,12 @@ def has_not_role(firma_name):
 # return True if the company name has the specified role, false otherwise
 def is_role_in_name(role, name):
     try:
-        index = name.index(role, len(name) - len(role))
+        index = name.index(role, len(name) - len(role))  # get index of searched role from end of string
         if (name[index - 1] == " ") and ((index + len(role)) % len(name) == 0):
             return True
     except:
         try:
-            index = name.index(role)
+            index = name.index(role)  # if searched role was not found at the end of string
             if (name[index-1] == " ") and (name[index+len(role)] == " "):
                 return True
         except:
@@ -99,7 +99,7 @@ def is_role_in_name(role, name):
 
 
 # Parse the XML Elements into objects and populate the given list
-# @param firma_element the XML root element
+# @param firma_element the XML Firma element
 # @param firmen_list the list to be populated
 # return the populated list
 def get_firmen_list(firma_element, firmen_list):
@@ -115,23 +115,23 @@ def get_firmen_list(firma_element, firmen_list):
         'adresszusatz').text
     firma.erfassungsdatum = firma_element.find('erfassungsdatum').text
     firma.ansprechpartner = []
-    ansprechpartnern = firma_element.findall('ansprechpartner')
+    ansprechpartnern_list = firma_element.findall('ansprechpartner')
 
-    for ansprechpartner_element in ansprechpartnern:
-        ansprechpartner = Ansprechpartner()
-        ansprechpartner.name = ansprechpartner_element.attrib['name']
-        ansprechpartner.anrede = ansprechpartner_element.find('anrede').text
-        ansprechpartner.titel = "" if ansprechpartner_element.find('titel') is None else \
+    for ansprechpartner_element in ansprechpartnern_list:
+        ansprechpartner_object = Ansprechpartner()
+        ansprechpartner_object.name = ansprechpartner_element.attrib['name']
+        ansprechpartner_object.anrede = ansprechpartner_element.find('anrede').text
+        ansprechpartner_object.titel = "" if ansprechpartner_element.find('titel') is None else \
             ansprechpartner_element.find('titel').text
-        ansprechpartner.funktion = "" if ansprechpartner_element.find('funktion') is None else \
+        ansprechpartner_object.funktion = "" if ansprechpartner_element.find('funktion') is None else \
             ansprechpartner_element.find('funktion').text
-        ansprechpartner.telefon = ansprechpartner_element.find('telefon').text
-        ansprechpartner.fax = "" if ansprechpartner_element.find('fax') is None else \
+        ansprechpartner_object.telefon = ansprechpartner_element.find('telefon').text
+        ansprechpartner_object.fax = "" if ansprechpartner_element.find('fax') is None else \
             ansprechpartner_element.find('fax').text
-        ansprechpartner.email = ansprechpartner_element.find('email').text
+        ansprechpartner_object.email = ansprechpartner_element.find('email').text
         # declare the object as dictionary in order to be able to be parsed into JSON
-        ansprechpartner = ansprechpartner.__dict__
-        firma.ansprechpartner.append(ansprechpartner)
+        ansprechpartner_object = ansprechpartner_object.__dict__
+        firma.ansprechpartner.append(ansprechpartner_object)
     # same as above
     firma = firma.__dict__
     firmen_list.append(firma)
--- server.py
+++ server.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, Response,redirect
+from flask import Flask, request, Response, redirect
 from Service.Parser import get_firmen_JSON
-from Config.format import post_string,pre_string
+from Config.format import post_string, pre_string
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return redirect("/firmen")
+
 
 @app.route('/firmen', methods=['GET'])
 def get():
@@ -15,10 +17,10 @@ def get():
     ort = request.args.get('ort', '').lower().strip()
     rolle = request.args.get('rolle', '').lower().strip()
     if name == "" and branche == "" and ort == "" and rolle == "":
-        return Response("<h3>Keine Parameter wurden eingegeben</h3>", status=400,mimetype="text/html")
+        return Response("<h3>Keine Parameter wurden eingegeben</h3>", status=400, mimetype="text/html")
     firmen_list = get_firmen_JSON(name, branche, ort, rolle)
-    return Response(pre_string+firmen_list+post_string, mimetype="application/json", status=200)
+    return Response(pre_string + firmen_list + post_string, mimetype="application/json", status=200)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True)
+    app.run()
